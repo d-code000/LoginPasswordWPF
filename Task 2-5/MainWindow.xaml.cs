e89995a41c31204f9756dc6505406291e4fcd3fe
Disha <disha.wind@gmail.com>
2025-04-16 17:14:46 +0300

    catch login duplicate exceptions

--- Task 2-5/MainWindow.xaml.cs
+++ Task 2-5/MainWindow.xaml.cs
@@ -1,65 +1,74 @@
 ﻿using System.Windows;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
-namespace Task_2_5
+namespace Task_2_5;
+
+public partial class MainWindow
 {
-    public partial class MainWindow
+    private readonly ApplicationContext _db;
+
+    public MainWindow()
     {
-        private readonly ApplicationContext _db;
+        InitializeComponent();
+        _db = new ApplicationContext();
+        _db.EnsureDbCreated();
+    }
 
-        public MainWindow()
+    private void Button_Register_Click(object sender, RoutedEventArgs e)
+    {
+        string login = LoginTextBox.Text.Trim();
+        string pass = PassBox.Password.Trim();
+            
+        if (login.Length < 3)
         {
-            InitializeComponent();
-            _db = new ApplicationContext();
-            _db.EnsureDbCreated();
+            MessageBox.Show("Логин должен содержать не менее 3 символов!");
+            return;
         }
 
-        private void Button_Register_Click(object sender, RoutedEventArgs e)
+        if (pass.Length < 6)
         {
-            string login = LoginTextBox.Text.Trim();
-            string pass = PassBox.Password.Trim();
+            MessageBox.Show("Пароль должен содержать не менее 6 символов!");
+            return;
+        }
             
-            if (login.Length < 3)
-            {
-                MessageBox.Show("Логин должен содержать не менее 3 символов!");
-                return;
-            }
-
-            if (pass.Length < 6)
-            {
-                MessageBox.Show("Пароль должен содержать не менее 6 символов!");
-                return;
-            }
-            
-            if (pass.Contains("*"))
-            {
-                MessageBox.Show("Пароль не должен содержать символ '*'!");
-                return;
-            }
-
-            try
-            {
-                User user = new User(login, pass);
-                
-                _db.Users.Add(user);
-                _db.SaveChanges();
-
-                MessageBox.Show("Пользователь успешно зарегистрирован!");
-                
-                AuthWindow authWindow = new AuthWindow();
-                authWindow.Show();
-                Hide();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ошибка при регистрации: " + ex.Message);
-            }
+        if (pass.Contains("*"))
+        {
+            MessageBox.Show("Пароль не должен содержать символ '*'!");
+            return;
         }
 
-        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        try
         {
+            User user = new User(login, pass);
+                
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            MessageBox.Show("Пользователь успешно зарегистрирован!");
+                
             AuthWindow authWindow = new AuthWindow();
             authWindow.Show();
             Hide();
         }
+            
+        catch (DbUpdateException ex) when (
+            ex.InnerException is SqliteException { SqliteErrorCode: 19 } sqliteEx &&
+            sqliteEx.Message.Contains("UNIQUE constraint failed: Users.Login"))
+        {
+            MessageBox.Show("Логин уже используется. Пожалуйста, выберите другой.");
+        }
+            
+        catch (Exception ex)
+        {
+            MessageBox.Show("Ошибка при регистрации: " + ex.Message);
+        }
+    }
+
+    private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+    {
+        AuthWindow authWindow = new AuthWindow();
+        authWindow.Show();
+        Hide();
     }
 }
